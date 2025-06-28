e38096ea802f0f9483bc8f7729fc2196a9e05c00
Matias Lucewicz <matyhas@gmail.com>
2025-06-28 03:14:41 -0300

    corregido venv, agregado models y admin,  y creado superuser

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from portfolio import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('recetas', views.recetas, name='recetas'),
     path('contacto', views.contacto, name='contacto'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Receta(models.Model):
+    titulo = models.CharField(max_length=100)
+    foto = models.ImageField(upload_to='recetas/', blank=True, null=True)
+    receta = models.TextField
