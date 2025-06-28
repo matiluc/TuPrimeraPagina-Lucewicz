7fcc68e2b087e4e22a36e8b2b73cdbfff2366d3d
Matias Lucewicz <matyhas@gmail.com>
2025-06-28 03:28:59 -0300

    borrado .gitignore, creadas Clases de prueba, no funcionan falta makemigrations

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Receta
+from .models import Receta, Chef, Moderador
 
 admin.site.register(Receta)
+admin.site.register(Chef)
+admin.site.register(Moderador)
--- portfolio/models.py
+++ portfolio/models.py
@@ -3,4 +3,14 @@ from django.db import models
 class Receta(models.Model):
     titulo = models.CharField(max_length=100)
     foto = models.ImageField(upload_to='recetas/', blank=True, null=True)
-    receta = models.TextField
+    receta = models.TextField()
+
+class Chef(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+
+class Moderador(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
