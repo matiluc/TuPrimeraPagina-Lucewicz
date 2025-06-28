9ef59bdb15d68a675547d249c4b3c8c4a629eb4e
Matias Lucewicz <matyhas@gmail.com>
2025-06-28 19:00:47 -0300

    la carga de items desde la bbdd funciona pero falta darle estilo

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Receta(models.Model):
     titulo = models.CharField(max_length=100)
     foto = models.ImageField(upload_to='recetas/', blank=True, null=True)
-    receta = models.TextField()
+    receta = RichTextField()
 
     def __str__(self):
         return self.titulo
